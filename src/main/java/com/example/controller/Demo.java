dc60a4173ceeafe2d1556bca88a97571516119da
wuhan09 <2957464339@qq.com>
2019-01-06 17:45:39 +0800

    新建分支代码开发

---
+++ src/main/java/com/example/controller/Demo.java
@@ -0,0 +1,10 @@
+package com.example.controller;
+
+/**
+ * Created by zhuhanchao on 2019/1/6.
+ */
+public class Demo {
+    public static void main(String[] args) {
+        System.out.println("新建hot_fix分支");
+    }
+}
